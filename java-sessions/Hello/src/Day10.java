2a6d8e88356520bfaab55cd37aa788d3f4960e9d
Aravindhsiva <adamaravindh@gmail.com>
2026-06-16 10:26:49 +0530

    Adding ArrayList

---
+++ java-sessions/Hello/src/Day10.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Day10 {
+    static void main() {
+        String[] alpha = {"A","B","C","D"};
+        System.out.println(alpha.length);
+
+        ArrayList<String> list = new ArrayList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        for(String s: list) {
+            System.out.print(s);
+        }
+        list.add("E");
+
+        list.remove("C");
+        System.out.println();
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println();
+        for(String s: list) {
+            System.out.print(s);
+        }
+        System.out.println();
+        System.out.println(list.get(2));
+        list.addAll(list);
+
+        System.out.println();
+
+        for(String s: list) {
+            System.out.print(s);
+        }
+    }
+
+}
