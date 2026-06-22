c16151ed336cdec04e79b5a038d64797820a1441
Aravindhsiva <adamaravindh@gmail.com>
2026-06-22 16:15:52 +0530

    Adding Progress for Day 14 Strategy and Adapter Pattern

---
+++ java-sessions/Hello/src/Day14_Adapter.java
@@ -0,0 +1,29 @@
+public class Day14_Adapter {
+
+    interface NewPrinter {
+        void print(String text);
+    }
+
+    static class OldPrinter {
+        void printText(String text) {
+            System.out.println("OLD Printer : "+text);
+        }
+    }
+
+    static class OldPrinterAdapter implements NewPrinter {
+        private final OldPrinter printer;
+        OldPrinterAdapter(OldPrinter printer) {
+            this.printer = printer;
+        }
+
+        @Override
+        public void print(String text){
+            printer.printText(text);
+        }
+    }
+
+    static void main() {
+        NewPrinter printer = new OldPrinterAdapter(new OldPrinter());
+        printer.print("Hello World");
+    }
+}
